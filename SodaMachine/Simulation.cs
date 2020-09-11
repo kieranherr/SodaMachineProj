2cea2163277ed2ca40ffd19733f48f8a821eb0b6
Kieran <Kieranherr@gmail.com>
2020-09-10 19:11:28 -0500

    Finished the 25 point user story dealing with dispensing change and soda. Also added the ability to choose which coins to pay with.

--- SodaMachine/Simulation.cs
+++ SodaMachine/Simulation.cs
@@ -19,32 +19,84 @@ namespace SodaMachine
         }
         public void CheckMoney(Can can, List<Coin> coins, double totalCoinValueInserted)
         {
-            if (can.Cost > totalCoinValueInserted)
+            double regAmt = 0;
+            foreach(Coin c in sodaMachine.register)
             {
-                Console.WriteLine("not enough coins entered");
-                Console.ReadLine();
-                customer.wallet.coins.AddRange(coins);
+                regAmt = c.Amount + regAmt;
             }
-            else if (can.Cost == totalCoinValueInserted)
+            if (totalCoinValueInserted < regAmt)
             {
-                //no change returned, just dispense the soda
-                Console.WriteLine("Give Soda");
-                Console.ReadLine();
-                sodaMachine.inventory.Remove(can);
-                customer.backpack.cans.Add(can);
-                customer.backpack.cans.ForEach(Console.WriteLine);
+                if (can.Cost > totalCoinValueInserted)
+                {
+                    Console.WriteLine("not enough coins entered");
+                    Console.ReadLine();
+                    customer.wallet.coins.AddRange(coins);
+                }
+                else if (can.Cost == totalCoinValueInserted && sodaMachine.inventory.Contains(can))
+                {
+                    //no change returned, just dispense the soda
+                    Console.WriteLine("Give Soda");
+                    Console.ReadLine();
+                    sodaMachine.inventory.Remove(can);
+                    customer.backpack.cans.Add(can);
+                    customer.backpack.cans.ForEach(Console.WriteLine);
 
+                }
+                else if (can.Cost < totalCoinValueInserted && sodaMachine.register.Count != 0 && sodaMachine.inventory.Contains(can))
+                {
+                    //return the change left over after minusing the cost of the can
+                    //dispense soda as well
+                    Console.WriteLine("Dispense and also give change");
+                    Console.ReadLine();
+                    sodaMachine.inventory.Remove(can);
+                    customer.backpack.cans.Add(can);
+                    double change = totalCoinValueInserted - can.Cost;
 
+                }
+                else if (sodaMachine.inventory.Contains(can) == false)
+                {
+                    foreach (Coin c in coins)
+                    {
+                        customer.wallet.coins.Add(c);
+                    }
+                }
             }
-            else if(can.Cost < totalCoinValueInserted && sodaMachine.register.Count != 0 && sodaMachine.inventory.Contains(can))
+            else
             {
-                //return the change left over after minusing the cost of the can
-                //dispense soda as well
-                Console.WriteLine("Dispense and also give change");
-                Console.ReadLine();
+                foreach (Coin c in coins)
+                {
+                    customer.wallet.coins.Add(c);
+                }
             }
-
             
+        }
+        public List<Coin> ChooseCoins(Coin coin, string coinName)
+        {
+            List<Coin> coins = new List<Coin>();
+            while (coinName != "stop")
+            {
+                if (coinName == "quarter")
+                {
+                    coin = new Quarter();
+                    coins.Add(coin);
+                }
+                else if (coinName == "nickel")
+                {
+                    coin = new Nickle();
+                    coins.Add(coin);
+                }
+                else if (coinName == "dime")
+                {
+                    coin = new Dime();
+                    coins.Add(coin);
+                }
+                else if (coinName == "penny")
+                {
+                    coin = new Penny();
+                    coins.Add(coin);
+                }
+            }
+            return coins;
         }
     }
 }
--- SodaMachine/UserInterface.cs
+++ SodaMachine/UserInterface.cs
@@ -30,5 +30,6 @@ namespace SodaMachine
        //         Console.ReadLine();
        //     }
        // }
+
     }
 }
